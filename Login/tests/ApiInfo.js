0eb079e90a25dbb9b583e35ebf233227c93ffc10
TeraWill <newmeta.will@gmail.com>
2023-05-12 17:34:37 -0300

    Pegando autorização da API usando Cookies

---
+++ Login/tests/ApiInfo.js
@@ -0,0 +1,96 @@
+// Classe de validação da chave de acesso + liberação do JSON
+
+class ApiInfo{
+
+  auth
+
+  getAuth(){
+    return this.auth
+  }
+  
+  // Atualiza o um novo código para o auth
+  async GetNewToken(){ // Retorna uma nova autorização
+
+    let token;
+    const apiKey = ["x-api-key", "f6d7f7abb1ff0e3b1557db73427f33912a514cd63c0aeec9ae"];
+    const loginData = [["login", "jogodeouro"], ["password", "2w308efh"]]; // Inutil por enquanto, mas pode ser útil
+
+    var Head = new Headers();
+    Head.append(apiKey[0], apiKey[1]);
+
+    var Body = new FormData();
+    Body.append("login", "jogodeouro");
+    Body.append("password", "2w308efh");
+
+    const requestOptions = {
+      method: 'POST',
+      headers: Head,
+      body: Body,
+      redirect: 'follow'
+    };
+
+    try { // Tenta utilizar a API e colocar na variável Token e retornar ele
+      const response = await fetch("https://apiv2dev.sga.bet/integrations/user/login", requestOptions);
+      const data = await response.json();
+      token = data["token_jwt"];
+      return token;
+    } 
+    catch (error) {
+      console.log(error);
+    }
+    return String(token);
+  }
+
+  // Funcção principal que insere valor e atualiza o auth
+  async setAuthCookie(){
+    const nameCookie = "sgaApiKey"
+    // const timeCookie = 4.5 * 60 * 60 * 1000
+    const timeCookie = .1 * 60 * 60 * 1000
+
+    if(!cookieExists(nameCookie)[0]){
+      this.auth = await this.GetNewToken()
+      setCookie(nameCookie, this.auth, timeCookie)
+      console.log("Cookie atualizado")
+    }
+    else{
+      getCookie(nameCookie)
+      console.log("Cookie alcançado")
+    }
+  }
+}
+
+
+// Classe de cookies + alguma coisa...?
+const apiInfo = new ApiInfo()
+
+
+function cookieExists(cookieName) {
+  var cookies = document.cookie;
+  var cookiesArray = cookies.split(';');
+
+  for (var cont = 0; cont < cookiesArray.length; cont++) {
+    var cookie = cookiesArray[cont].trim();
+    console.log(cookiesArray) // Teste
+    if (cookie.startsWith(cookieName + '=')) {
+      return [true, cookie]; // Cookie Existe
+    }
+  }
+  return false; // Cookie non Ecxiste
+}
+  // Retorna o cookie pelo NamedNodeMap, só n sei se retorna cookie vencido
+function getCookie(cookieName){
+
+  var cookieData = cookieExists(cookieName);
+
+  if(cookieData[0]){
+    return cookieData[1].split('=')[1];
+    }
+}
+
+function setCookie(name, value, valDate){
+  let cookieName = String(name);
+  let cookieValue = value
+  let cookieTime = new Date(Date.now() + Number(valDate)); 
+
+  document.cookie = `${cookieName}=${cookieValue}; expires=${cookieTime.toUTCString()}; path=localhost:8000; httponly=true; SameSite=none; secure`
+}
--- Login/tests/index.php
+++ Login/tests/index.php
@@ -1,17 +1,13 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Teste</title>
+	<title>Exemplo</title>
 </head>
 <body>
-    <h1 class="title">
-
-    </h1>
-    <input type="button" value="Me Aperta Poh" onclick="testLoop()">
-    <input type="button" value="Mostrar" onclick="logBook(y)">
-    <script src="test.js"></script>
+	<h1 class="title"></h1>
+	<input type="button" value="Me Aperta Poh" onclick="apiInfo.setAuthCookie()">
+	<script src="ApiInfo.js"></script>
+	<script>
+	</script>
 </body>
 </html>
