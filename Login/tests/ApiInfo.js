90ddaad976cd86a19ee5d5348e77b55f1fecfcf8
TeraWill <newmeta.will@gmail.com>
2023-05-16 18:20:57 -0300

    Criando uma classe para uso fácil da API

--- Login/tests/ApiInfo.js
+++ Login/tests/ApiInfo.js
@@ -31,25 +31,35 @@ class CookieHandler{
 }
 
 const ch = new CookieHandler();
+
 // Classe de validação da chave de acesso + liberação do JSON
 class ApiInfo{
 
-// Variável Privada que é retornada na classe como autorização da API
-  constructor(){
-    let _auth;
+  constructor() {
+    this.authHandlerInstance = this.authHandler();
+  }
 
-    this.getAuth = () => {
-      return _auth;
-    }
-    this.setAuth = (valor) => {
-      _auth = valor;
-    }
+// Variável Privada que é retornada na classe como autorização da API
+  authHandler() {
+    let auth;
+
+    return {
+      getAuth: async () => {
+        console.log(auth);
+        return auth;
+
+      },
+
+      setAuth: (valor) => {
+        auth = valor;
+      }
+    };
   }
 
   // Atualiza o um novo código para o auth
   async  GetNewToken(){ // Retorna uma nova autorização
     
-    let token;
+    let token = "Teste";
 
     const apiKey = ["x-api-key", "f6d7f7abb1ff0e3b1557db73427f33912a514cd63c0aeec9ae"];
     const loginData = [["login", "jogodeouro"], ["password", "2w308efh"]]; // Inutil por enquanto, mas pode ser útil
@@ -77,25 +87,84 @@ class ApiInfo{
     catch (error) {
       console.log(error);
     }
-    // return token;
   }
 
-// Função principal que insere valor e atualiza o auth
+// Função principal que insere valor no Cookie e/ou atualiza o auth
   async setAuthCookie() {
     const nameCookie = "sgaApiKey";
-    const timeCookie = .1 * 60 * 60 * 1000; // 6 min
+    const timeCookie = .05 * 60 * 60 * 1000; // 6 min
     // const timeCookie = 5 * 60 * 60 * 1000; // 5h
+    var cookieCheck = ch.cookieExists(nameCookie)
+    let token = this.authHandlerInstance.getAuth()
 
-    if (!ch.cookieExists(nameCookie)[0]) {
-      this.setAuth(await this.GetNewToken());
-      ch.setCookie(nameCookie, this.getAuth(), timeCookie);
-
+    if (!cookieCheck[0]) {
+      token = await this.GetNewToken()
+      this.authHandlerInstance.setAuth(token);
+      ch.setCookie(nameCookie, token, timeCookie);
+      console.log("Cookie Atualizado")
     } 
     else {
       ch.getCookie(nameCookie);
-      console.log("Cookie Detectado")
+      this.authHandlerInstance.setAuth(cookieCheck[1])
+      console.log("Cookie Detectado. mantendo a autorização para: " + await this.authHandlerInstance.getAuth())
     }
   }
 
+  async useApi(dateStart, dateEnd, infoList = 0){
+    let table = "Teste";
+    await this.setAuthCookie();
 
+    const apiKey = ["x-api-key", "f6d7f7abb1ff0e3b1557db73427f33912a514cd63c0aeec9ae"];
+    const headerRequest = ['Application-Authorization', await this.authHandlerInstance.getAuth()]
+
+    switch(infoList){
+      case 0: // Usuários
+        infoList = "https://apiv2dev.sga.bet/integrations/players/listInfos" 
+        dateStart = ["start_date", dateStart]
+        dateEnd = ["final_date", dateEnd]
+      break
+
+      case 1: // Apostas esportivas
+        infoList = "https://apiv2dev.sga.bet/integrations/bets/list"
+        dateStart = ["date_start", dateStart]
+        dateEnd = ["date_final", dateEnd]
+      break
+
+      case 2: // Apostas Cassino
+        infoList = "https://apiv2dev.sga.bet/integrations/bets/casino"
+        dateStart = ["start_date", dateStart]
+        dateEnd = ["final_date", dateEnd]
+      break
+      default:
+        console.log("Erro ao informar a lista desejada")
+      break
+    }
+
+    var Head = new Headers();
+      Head.append(apiKey[0], apiKey[1]); 
+      Head.append(headerRequest[0], headerRequest[1]);
+
+    var Body = new FormData();
+      Body.append(dateStart[0], dateStart[1]);
+      Body.append(dateEnd[0], dateEnd[1]);
+
+    const requestOptions = {
+      method: 'POST',
+      headers: Head,
+      body: Body,
+      redirect: 'follow'
+    };
+    
+    try { // Tenta utilizar a API e colocar na variável table e retornar ele
+      const response = await fetch(infoList, requestOptions)
+      const data = await response.json()
+      table = data
+      console.log(table)
+      return table
+    }
+    catch (error) {
+      console.log(error)
+    }
+    
+  }
 }
--- Login/tests/index.php
+++ Login/tests/index.php
@@ -5,7 +5,8 @@
 </head>
 <body>
 	<h1 class="title"></h1>
-	<input type="button" value="Me Aperta Poh" onclick="apiInfo.setAuthCookie() ">
+	<input type="button" value="Me Aperta Poh" onclick="apiInfo.useApi('2023-05-01 00:00:00' , '2023-05-01 23:59:59', 1)" >
+	<input type="button" value="Me Aperta Poh" onclick="apiInfo.setAuthCookie()" >
 	<script src="ApiInfo.js"></script>
 	<script>
 		let apiInfo = new ApiInfo();
